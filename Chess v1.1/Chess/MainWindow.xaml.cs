d2a02e3fd8290630918bf2c70eb134e0e63c6dba
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2022-12-25 17:59:35 +0100

    przebudowa przycisków

--- Chess v1.1/Chess/MainWindow.xaml.cs
+++ Chess v1.1/Chess/MainWindow.xaml.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
 using General;
 
 namespace Chess
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         App app;
@@ -33,11 +30,30 @@ namespace Chess
 
         private void PlayAction(object sender, RoutedEventArgs e)
         {
-            Grid chessGrid = new Grid();
-            chessGrid.ShowGridLines = true;
-            chessGrid.Margin = new Thickness(20, 20, 20, 20);
-            Image[,] boardImages = new Image[8, 8];
-            Button[,] boardClick = new Button[8, 8];
+            // ustawianie głównej siatki
+            Grid grid = new Grid()
+            {
+                ShowGridLines = true,
+                Margin = new Thickness(20, 20, 20, 20)
+            };
+
+            ColumnDefinition column1 = new ColumnDefinition
+            {
+                Width = new GridLength(100, GridUnitType.Star)
+            };
+            grid.ColumnDefinitions.Add(column1);
+            ColumnDefinition column2 = new ColumnDefinition
+            {
+                Width = new GridLength(100, GridUnitType.Star)
+            };
+            grid.ColumnDefinitions.Add(column2);
+
+            // ustawianie siatki szachów
+            Grid chessGrid = new Grid
+            {
+                ShowGridLines = false,
+                Margin = new Thickness(10, 10, 10, 10)
+            };
             // kolumny
             ColumnDefinition[] columns = new ColumnDefinition[8];
             for (int i = 0; i < 8; i++)
@@ -51,52 +67,40 @@ namespace Chess
             for (int i = 0; i < 8; i++)
             {
                 rows[i] = new RowDefinition();
-                rows[i].Height = new GridLength(100, GridUnitType.Star);
                 chessGrid.RowDefinitions.Add(rows[i]);
             }
 
             // Dodawanie pól
-            Button button;
+            AreaButton button;
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0; j < 8; j++)
                 {
-                    button = new Button();
+                    button = new AreaButton(ChessColor.White);
+                    button.Style = (Style)Resources["customButton"];
                     button.Click += BoardClick;
-                    button.Content = "area " + Convert.ToString(i) + " " + Convert.ToString(j);
-                    //Image img = new Image();
-                    //img.Source = new BitmapImage(new Uri("ErrorImage.png", UriKind.Relative));
+                    button.SourceSet("ErrorImage.png");
+
                     chessGrid.Children.Add(button);
                     Grid.SetColumn(button, i);
                     Grid.SetRow(button, j);
-
-                    //boardImages[i, j] = new Image();
-                    //boardImages[i, j].HorizontalAlignment = HorizontalAlignment.Left;
-                    //boardImages[i, j].VerticalAlignment = VerticalAlignment.Top;
-                    //boardImages[i, j].Width = 60;
-                    //boardImages[i, j].Height = 60;
-                    //boardImages[i, j].Margin = new Thickness(20 + 60 * i, 20 + 60 * j, 0, 0);
-                    //Uri dirtUri = new Uri("pack://aplication:,,,/");
-                    //boardImages[i, j].Source = new BitmapImage(new Uri("armor_stand.png", UriKind.Relative));
-                    //imageBoard[i, j].Source = bee.Source;
-                    //boardImages[i, j].CacheMode;
-                    //grid.Children.Add(boardImages[i, j]);
                 }
             }
+            grid.Children.Add(chessGrid);
+            Grid.SetColumn(chessGrid, 0);
 
+            // Stack panel i content controlls - definicje
             StackPanel contentPanel = new StackPanel();
-            //grid.Background = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/dirt.png", UriKind.Relative)));
-            contentPanel.Children.Add(chessGrid);
+            contentPanel.Children.Add(grid);
 
             menuControlls = new ContentControl();
             menuControlls.Content = this.Content;
             this.Content = contentPanel;
-
-            MessageBox.Show("Ta funkcja nie zastała jeszcze zaimplementowana!", "Funkcja nie istnieje", MessageBoxButton.OK, MessageBoxImage.Information);
         }
         private void BoardClick(object sender, RoutedEventArgs e)
         {
-            // pass
+            // brak 
+            MessageBox.Show("Ta funkcja nie zastała jeszcze zaimplementowana!", "Funkcja nie istnieje", MessageBoxButton.OK, MessageBoxImage.Information);
         }
         private void PlayOnlineAction(object sender, RoutedEventArgs e)
         {
--- Chess v1.1/Chess/MenuButton.cs
+++ Chess v1.1/Chess/MenuButton.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿//using System;
 using System.Windows.Controls;
-using System.Collections.Generic;
-using System.Text;
+//using System.Collections.Generic;
+//using System.Text;
 using System.Windows.Media;
 using System.Windows.Input;
 
